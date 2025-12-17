2829d75b7a8de1cb3df4817b38a3fac175f18a59
sunilsimar <sunilsimar@users.noreply.github.com>
2025-12-17 20:21:33 +0530

    feat: add blog state machine

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_reveiew();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+
+    post.reject();
+    assert_eq!("", post.content());
+}
